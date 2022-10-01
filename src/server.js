b8d61c878d1ca26ee526631b3d91132f593781d0
Steve <steve@MacBook-Air-3.local>
2022-10-01 16:53:11 +0300

    Config mongodb, use invironment var

--- src/server.js
+++ src/server.js
@@ -1,15 +1,15 @@
 import express from "express";
-import { mapOrder } from "*/utilities/sorts.js";
+import { env } from "*/config/environment";
+import { connectDB } from "*/config/mongodb";
 
 const app = express();
 
-const hostname = "localhost";
-const port = 8080;
+connectDB().catch(console.log);
 
 app.get("/", (req, res) => {
     res.end("<h1>Hello world!</h1><hr/>");
 });
 
-app.listen(port, hostname, () => {
-    console.log(`Hello Hieu, I'm running at ${hostname} : ${port}`);
+app.listen(env.PORT, env.HOST, () => {
+    console.log(`Hello Hieu, I'm running at ${env.HOST} : ${env.PORT}`);
 });
